4d9e02b01fdd691a2e553bf3babb487c41dbd976
Ty3uK <max.karelov@gmail.com>
2015-10-27 23:20:43 +0300

    Added 'comming soon' page

---
+++ bundle.js
@@ -0,0 +1,1 @@
+!function(t){function n(r){if(o[r])return o[r].exports;var e=o[r]={exports:{},id:r,loaded:!1};return t[r].call(e.exports,e,e.exports,n),e.loaded=!0,e.exports}var o={};return n.m=t,n.c=o,n.p="/",n(0)}({0:function(t,n,o){o(1),t.exports=o(12)},1:function(t,n,o){o(2)},2:function(t,n){},12:function(t,n,o){t.exports=o.p+"index.html"}});
